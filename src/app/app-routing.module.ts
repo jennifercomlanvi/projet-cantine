f06e1889abce42b0943438ed597fc1e454f6a2c3
comlanvi jennifer <comlanviida@yahoo.fr>
2022-11-25 09:46:16 +0100

    'init'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConnexionComponent } from './components/connexion/connexion.component';
+import { InscriptionComponent } from './components/inscription/inscription.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'connexion', component:ConnexionComponent},
+  {path:'inscription', component:InscriptionComponent},
+  {path:'', redirectTo: 'connexion', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
