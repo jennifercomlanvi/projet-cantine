764802436252e6b98f01d4c2183cdbc65245da9e
22Tayeb <tayeb@adame-techs.com>
2022-12-21 16:17:12 +0100

    j'ai juste rediriger le menu vers une page html et ajouter du texte

---
+++ src/assets/js/menu.js
@@ -0,0 +1,5 @@
+var maintenant=new Date();
+var jour=maintenant.getDate();
+var mois=maintenant.getMonth()+1;
+var an=maintenant.getFullYear();
+document.write("Nous sommes le ",jour,"/",mois,"/",an,".");
