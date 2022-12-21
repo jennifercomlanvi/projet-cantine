d12ee2ea90c6f17e7170393ff05128a0b55a3978
22Tayeb <tayeb@adame-techs.com>
2022-12-21 16:28:18 +0100

    mon travail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConnexionComponent } from './components/connexion/connexion.component';
 import { InscriptionComponent } from './components/inscription/inscription.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 const routes: Routes = [
   {path:'connexion', component:ConnexionComponent},
   {path:'inscription', component:InscriptionComponent},
+  {path:'menu', component:MenuComponent},
   {path:'', redirectTo: 'connexion', pathMatch:'full'}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConnexionComponent } from './components/connexion/connexion.component';
 import { InscriptionComponent } from './components/inscription/inscription.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConnexionComponent,
-    InscriptionComponent
+    InscriptionComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
