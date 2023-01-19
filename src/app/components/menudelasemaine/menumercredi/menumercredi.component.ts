379b2e3352715a531ce0953a919796043d6905d8
22Tayeb <comlanviida@yahoo.fr>
2023-01-19 17:55:25 +0100

    menudelas fini

--- src/app/components/menudelasemaine/menumercredi/menumercredi.component.ts
+++ src/app/components/menudelasemaine/menumercredi/menumercredi.component.ts
@@ -1,4 +1,9 @@
+import { Router } from '@angular/router';
+import { MenusemaineService } from './../menusemaine.service';
 import { Component, OnInit } from '@angular/core';
+import { ingrediends } from '../../menuduJ/ingredients';
+import { meal } from '../../menuduJ/meal';
+import { MenuDJ } from '../../menuduJ/menudj';
 
 @Component({
   selector: 'app-menumercredi',
@@ -6,10 +11,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menumercredi.component.scss']
 })
 export class MenumercrediComponent implements OnInit {
-
-  constructor() { }
+menudj!:MenuDJ[];
+  menu: MenuDJ= new MenuDJ();
+  mealdj!:meal[];
+  meal: meal= new meal();
+  ingredientdj!:ingrediends[];
+  ingrediend : ingrediends = new ingrediends();
+  constructor(private menuService:MenusemaineService, route:Router) { }
 
   ngOnInit(): void {
+    this.menuService.getMenumercredi().subscribe(data=>{
+      console.log(data)
+      if(data){
+        this.menudj = data
+        console.log(this.menudj)
+        for(let i of this.menudj)
+        {
+          this.mealdj = i.meals
+          for(let j of  this.mealdj)
+          {
+            this.ingredientdj = j.ingredients
+          }
+        }
+        console.log(this.mealdj)
+      }
+    })
   }
 
 }
+
+
